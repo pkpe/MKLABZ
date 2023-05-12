fe51c828ca244b56afef3a9085f0fc62028c1517
pkpe <94029038+pkpe@users.noreply.github.com>
2023-05-11 17:30:28 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+const connectWalletButton = document.getElementById("myButton");
+
+function connectWallet() {
+  // TODO: Implement connection to the Solana wallet
+  alert("Connecting wallet...");
+}
+
+connectWalletButton.addEventListener("click", connectWallet);
